7616b62f5d4f6c39b8369f5c440ebaf070e31aa2
Cheng Shi <cshi@ualberta.ca>
2018-10-11 20:06:39 +0000

    organize admin controller

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,4 @@
-class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASS']
+class Admin::DashboardController < AdminController
   def show
   end
 end
