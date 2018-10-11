c255897fd008e57497852f719236843fc54fd6b8
Cheng Shi <cshi@ualberta.ca>
2018-10-11 03:38:01 +0000

    add user auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+# handle login page render
+  def new
+  end
+
+  # handle login post
+  def create
+    @user = User.find_by_email(params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to '/'
+    else
+      render 'new'
+    end
+  end
+
+  # handle logout
+  def destroy
+    session[:user_id] = nil
+    redirect_to login_path
+  end
+
 end
