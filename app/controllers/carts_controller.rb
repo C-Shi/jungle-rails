b7a82b676c821b3c799e9d3124adf662f459caaa
Cheng Shi <cshi@ualberta.ca>
2018-10-12 20:31:05 +0000

    modify cart controller so add/remove will change db quantity and will track if add/remove is valid

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -7,7 +7,12 @@ class CartsController < ApplicationController
 
   def add_item
     product_id = params[:product_id].to_s
-    modify_cart_delta(product_id, +1)
+    if validate_quantity(product_id)
+      modify_cart_delta(product_id, + 1)
+      update_stock_quantity(product_id, 1)
+    else
+      flash[:danger] = "This product is currently out of stock"
+    end
 
     redirect_to :back
   end
@@ -15,7 +20,7 @@ class CartsController < ApplicationController
   def remove_item
     product_id = params[:product_id].to_s
     modify_cart_delta(product_id, -1)
-
+    update_stock_quantity(product_id, -1)
     redirect_to :back
   end
 
@@ -25,6 +30,17 @@ class CartsController < ApplicationController
     cart[product_id] = (cart[product_id] || 0) + delta
     cart.delete(product_id) if cart[product_id] < 1
     update_cart cart
+  end
+
+  def validate_quantity(product_id)
+    product = Product.find(product_id)
+    return product.quantity > 0
+  end
+
+  def update_stock_quantity(product_id, delta)
+    product = Product.find(product_id)
+    new_quantity = product.quantity - delta
+    product.update(quantity: new_quantity)
   end
 
 end
