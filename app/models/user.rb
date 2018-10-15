fad5012b80db6ab93574aa66c7cbbacb5bf3929b
Cheng Shi <cshi@ualberta.ca>
2018-10-15 20:14:17 +0000

    add custom auth class method

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,22 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :password_digest, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validate :password_length
+
+  def self.authenticate_with_credentials (email, password)
+    @user = self.find_by(email: email)
+    if @user && @user.authenticate(password)
+      @user
+    else
+      nil
+    end
+  end
+
+  private
+  def password_length
+    if (password.length < 6)
+      errors.add(:password, "must be 6 digits or more")
+    end
+  end
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "Validate" do
+
+    it "should not be valid if password does not match" do
+      user = {first_name: "Bob", last_name: "Smith", email: "bob@bob.com", password:"123456", password_confirmation: "321321"}
+      @user = User.new(user)
+      @user.save
+      expect(@user).not_to be_valid
+    end
+
+    it "should not be valid if duplicate email signup" do
+      user2 = {first_name: "Bob", last_name: "Smith", email: "Bob@bob.COM", password:"123456", password_confirmation: "123456"}
+      user1 = {first_name: "Bob", last_name: "Smith", email: "bob@bob.com", password:"123456", password_confirmation: "123456"}
+      @user1 = User.new(user1)
+      @user1.save
+      @user2 = User.new(user2)
+      @user2.save
+      expect(@user1).to be_valid
+      expect(@user2).not_to be_valid
+    end
+
+    it "should not be valid if email is not present" do
+      user = {first_name: "Bob", last_name: "Smith", password:"123456", password_confirmation: "123456"}
+      @user = User.new(user)
+      @user.save
+      expect(@user).not_to be_valid
+    end
+
+    it "should not be valid if first_name is not present" do
+      user = {last_name: "Smith", email: "bob@bob.com", password:"123456", password_confirmation: "123456"}
+      @user = User.new(user)
+      @user.save
+      expect(@user).not_to be_valid
+    end
+
+    it "should not be valid if last_name is not present" do
+      user = {first_name: "Bob", email: "bob@gmail.com", password:"123456", password_confirmation: "123456"}
+      @user = User.new(user)
+      @user.save
+      expect(@user).not_to be_valid
+    end
+
+    it "should not be valid if password length is less than 6 digits" do
+      user = {first_name: "Judy", last_name: "Smith", email: "judy@gmail.com", password:"1234", password_confirmation: "1234"}
+      @user = User.new(user)
+      @user.save
+      expect(@user).not_to be_valid
+    end
+  end
+end
